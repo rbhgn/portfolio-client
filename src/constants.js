e9f00a257155a08950acc031ef80ef1fd429d57e
rbhgn <robhgn@gmail.com>
2018-08-11 13:47:44 +0200

    Changed Url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const localStorageJwtKey = process.env.JWT_KEY || 'currentUserJwt'
-export const baseUrl = 'https://rbhgn-server.herokuapp.com/'
+export const baseUrl = 'https://rbhgn-server.herokuapp.com'
 // || 'http://localhost:4000'
