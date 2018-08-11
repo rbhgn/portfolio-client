c00e50881a794a6e266a52b127b2f02535cdb1be
rbhgn <robhgn@gmail.com>
2018-08-11 13:10:59 +0200

    Changed environment variables

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,5 @@
 // export const baseUrl = 'http://localhost:4000'
-
-
 // export const baseUrl = 'https://rbhgn-server.herokuapp.com'
-export const localStorageJwtKey = 'currentUserJwt'
 
-export const baseUrl = process.env.DATABASE_URL || 'http://localhost:4000'
+export const localStorageJwtKey = process.env.JWT_KEY || 'currentUserJwt'
+export const baseUrl = process.env.SERVER_URL || 'http://localhost:4000'
