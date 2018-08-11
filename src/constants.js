75cdb12a9377705be0fd02bff4031617e5891ad3
rbhgn <robhgn@gmail.com>
2018-08-11 13:37:50 +0200

    _

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const localStorageJwtKey = process.env.JWT_KEY || 'currentUserJwt'
-export const baseUrl = process.env.SERVER_URL || 'http://localhost:4000'
+export const baseUrl = process.env.SERVER_URL 
+// || 'http://localhost:4000'
